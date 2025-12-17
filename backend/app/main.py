cb6a79a5301a94dcf4b7f7092bf2538e1650408e
sptryogi <115679084+sptryogi@users.noreply.github.com>
2025-12-17 09:51:42 +0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,15 @@ from .utils import build_json_context
 import os
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(chat_router)
 
 os.makedirs("storage", exist_ok=True)
